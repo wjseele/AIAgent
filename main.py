db74ce04504ba6ded2c9adc20db9921284b32917
wjseele <wjseele@pm.me>
2025-06-12 16:38:06 +0200

    Made a verbose flag

--- main.py
+++ main.py
@@ -16,7 +16,12 @@ if len(sys.argv) < 2:
 user_prompt = sys.argv[1]
 messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
+verbose = False
+if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+    verbose = True
+
 response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if verbose:
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
