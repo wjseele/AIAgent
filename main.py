479a83d83f355aa7588c145da1db2dda5f0843b6
wjseele <wjseele@pm.me>
2025-06-12 16:40:24 +0200

    Need to learn to read instructions

--- main.py
+++ main.py
@@ -23,5 +23,6 @@ if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
 response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 if verbose:
     print(response.text)
+    print(f"User prompt: {user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
