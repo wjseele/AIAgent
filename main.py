ef6a75de58078f76a34ab159be9874b0252cd59c
wjseele <wjseele@pm.me>
2025-06-12 16:32:00 +0200

    Made a response list

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 load_dotenv()
@@ -12,10 +13,10 @@ if len(sys.argv) < 2:
     print("Usage: python3 main.py <prompt>")
     sys.exit(1)
 
-question = sys.argv[1]
-print(question)
+user_prompt = sys.argv[1]
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=question)
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 print(response.text)
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
