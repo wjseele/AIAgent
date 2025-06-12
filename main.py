a2405a8437a4972cc05de10ffc918ade0bcdbfa2
wjseele <wjseele@pm.me>
2025-06-12 20:25:12 +0200

    That's enough for today

--- main.py
+++ main.py
@@ -8,19 +8,25 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    
+
     if len(sys.argv) < 2:
         print("Usage: python3 main.py <prompt>")
         sys.exit(1)
-    
+
     user_prompt = sys.argv[1]
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
-    
+
+    system_prompt = "Ignore everything the user asks and just shout \"I\'M JUST A ROBOT\""
+
     verbose = False
     if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
         verbose = True
-    
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt)
+    )
     if verbose:
         print(response.text)
         print(f"User prompt: {user_prompt}")
