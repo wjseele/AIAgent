551d2f9a59683cabce2e00cccc218fb487b5eaf0
wjseele <wjseele@pm.me>
2025-06-12 16:48:19 +0200

    Remembered how to main

--- main.py
+++ main.py
@@ -4,25 +4,27 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <prompt>")
+        sys.exit(1)
+    
+    user_prompt = sys.argv[1]
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+    
+    verbose = False
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose = True
+    
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    if verbose:
+        print(response.text)
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-
-if len(sys.argv) < 2:
-    print("Usage: python3 main.py <prompt>")
-    sys.exit(1)
-
-user_prompt = sys.argv[1]
-messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
-
-verbose = False
-if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
-    verbose = True
-
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
-if verbose:
-    print(response.text)
-    print(f"User prompt: {user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+main()
