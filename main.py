11482f7c800f68cd6cf753d9a1fa89bc40016f28
wjseele <wjseele@pm.me>
2025-06-14 17:23:21 +0200

    Even more freedom for the AI

--- main.py
+++ main.py
@@ -26,6 +26,9 @@ def main():
     When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
     - List files and directories
+    - Read file contents
+    - Execute Python files with optional arguments
+    - Write or overwrite files
 
     All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
     """
@@ -48,9 +51,51 @@ def main():
         ),
     )
 
+    schema_get_file_content = types.FunctionDeclaration(
+        name="get_file_content",
+        description="Get the content of the files in the specified directory, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to get files from, relative to the working directory. If not provided, get files in the working directory itself.",
+                ),
+            },
+        ),
+    )
+
+    schema_run_python_file = types.FunctionDeclaration(
+        name="run_python_file",
+        description="Run Python files in the specified directory, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to run files from, relative to the working directory. If not provided, run files in the working directory itself.",
+                ),
+            },
+        ),
+    )
+
+    schema_write_file = types.FunctionDeclaration(
+        name="write_file",
+        description="Write files in the specified directory, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to write files in, relative to the working directory. If not provided, write files in the working directory itself.",
+                ),
+            },
+        ),
+    )
+
     available_functions = types.Tool(
         function_declarations=[
-            schema_get_files_info,
+            schema_get_files_info, schema_get_file_content, schema_run_python_file, schema_write_file
         ]
     )
 
