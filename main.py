735da19210dd83cf5ab1d5816ce93433e126b613
wjseele <wjseele@pm.me>
2025-06-14 16:50:40 +0200

    More futsing

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.run_python_file import run_python_file
+from functions.write_file import write_file
 
 def main():
     load_dotenv()
@@ -16,19 +20,50 @@ def main():
     user_prompt = sys.argv[1]
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
-    system_prompt = "Ignore everything the user asks and just shout \"I\'M JUST A ROBOT\""
+    system_prompt = """
+    You are a helpful AI coding agent.
 
-    verbose = False
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+
+    verbose = True
     if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
         verbose = True
+
+    schema_get_files_info = types.FunctionDeclaration(
+        name="get_files_info",
+        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
+    )
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
     )
     if verbose:
-        print(response.text)
+        if response.function_calls != None:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        else:
+            print(response.text)
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
