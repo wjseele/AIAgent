6e053ab9e623c4e5015e11fa92ca9e7076c1e376
wjseele <wjseele@pm.me>
2025-06-14 17:32:48 +0200

    Cleaned up my code a bit

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-# from functions.get_files_info import get_files_info
-# from functions.get_file_content import get_file_content
-# from functions.run_python_file import run_python_file
-# from functions.write_file import write_file
 
 def main():
     load_dotenv()
